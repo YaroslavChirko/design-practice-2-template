6e352298b67750d1a76f4119d77fcee4abb459b0
YaroslavChirko <31710480+YaroslavChirko@users.noreply.github.com>
2020-05-23 17:10:00 +0300

    Update godoc-binary_test.go

--- build/godocmodule/godoc-binary_test.go
+++ build/godocmodule/godoc-binary_test.go
@@ -8,7 +8,7 @@ import (
 func TestGoDoc(t *testing.T) {
 	
 	if _, err := os.Stat("../out/docs/my-docs.html"); err != nil {
-    		t.Errorf("File or path does not exist, error: %s",err);  
+    		t.Errorf("File or path doesn't exist, error: %s",err);  
   	}
 	
 }
